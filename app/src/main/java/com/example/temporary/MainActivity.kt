9d454fb322f49774a5b864cc9911aa0fdfea790b
Devendra <deven32784@gmail.com>
2021-10-27 20:16:22 +0530

    add text view in every fragments

--- app/src/main/java/com/example/temporary/MainActivity.kt
+++ app/src/main/java/com/example/temporary/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.temporary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.temporary.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val bottomNavigationView: BottomNavigationView = binding.bottomNavView
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        bottomNavigationView.setupWithNavController(navController)
+
     }
 }
